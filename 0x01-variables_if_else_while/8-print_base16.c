63892eacbb306b08a19bc656d4fcbf4f5f5e8966
HAJAR <elabdellaoui.hajar@gmail.com>
2023-03-17 13:45:48 +0000

    Print all numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function will print the all single digit
+ * numbers of base 16 starting from 0
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	do {
+		printf("%x", number++);
+	} while (number % 16 != 0);
+	putchar('\n');
+	return (0);
+}
