7cf775152038b14dd45e61a75640d10d3387ee72
HAJAR <elabdellaoui.hajar@gmail.com>
2023-03-21 12:39:08 +0000

    create a function that print all alphabet in lowercase and update header file with its prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lower alphabets
+ *
+ * Description: function to print the alphabets in lowercase
+ * Following by New ligne
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char lettre = 'a';
+
+	do {
+		_putchar(lettre++);
+	} while (lettre <= 'z');
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 # Welcome 0x02-functions_nested_loops directory
 We will create some examples of functions by using nested loops.
 0-putchar : program to print '_putchar' by using _putchar function
+1-alphabet.c : function print_alphabet that print all the alphabet in lowercase following by new line.
